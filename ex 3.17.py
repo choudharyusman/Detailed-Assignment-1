8c84240a32df32aca6d62d48382f16d593cd65c2
choudharyusman <45047731+choudharyusman@users.noreply.github.com>
2018-11-17 04:01:20 +0500

    Add files via upload

---
+++ ex 3.17.py
@@ -0,0 +1,18 @@
+print("Ex 3.17 (a)")
+x= eval('2*3+1')
+print(x)
+
+print("3.17 (b)")
+y= 'hello'
+print(y)
+
+print("3.17 (c)")
+a= 'hello'
+b= 'world!'
+print(str(a)+ ' ' + str(b))
+
+print("3.17 (d)")
+print('ASCII'.count('I'))
+
+print("3.17 (e)")
+print("x=5")
