e053b1bbc4dc340dd04532d7a613ad3cdc4f82e9
choudharyusman <45047731+choudharyusman@users.noreply.github.com>
2018-11-17 14:11:16 +0500

    Update ex 3.35.py

--- ex 3.35.py
+++ ex 3.35.py
@@ -4,5 +4,5 @@ def prob(n):
     no_heads= 2**-n
     return no_heads
 
-n= int(input("Enter a Non-negative Integer: "))
+n= int(input("Enter a Non-negative Integer ie Number of times the coin is tossed: "))
 print(prob(n))
